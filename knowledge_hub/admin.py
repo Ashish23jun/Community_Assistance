5669540083294cc39f8a568493ba48d24f875861
Ashish23jun <pandey.k.ashish.86@gmail.com>
2024-10-02 00:52:50 +0530

    new CHange

--- knowledge_hub/admin.py
+++ knowledge_hub/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from knowledge_hub.models import Query
-
-# Register your models here.
+#Register
 admin.site.site_header = "Community Assistance"
 admin.site.site_title = "Community Assistance Admin Portal"
 admin.site.index_title = "Welcome to Community Assistance Portal"
--- notifications/admin.py
+++ notifications/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from . import models
 
-
+#Admin
 @admin.register(models.Notification)
 class CustomQuestionAdmin(admin.ModelAdmin):
     pass
